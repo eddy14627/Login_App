f9c1929660525741b5935d92309ff8194d3c8518
Adarsh Mishra <adjee2020@gmail.com>
2024-02-14 03:33:07 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -20,21 +20,9 @@ app.use(cors());
 
 app.use("/api/users", userRoutes);
 app.use("/api/upload/upload-cloudinary", cloudinaryUpload);
-
-// const __dirname = path.resolve();
-// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/client/build")));
-
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-//   );
-// } else {
 app.get("/", (req, res) => {
   res.send("API is running....");
 });
-// }
 
 app.use(notFound);
 app.use(errorHandler);
