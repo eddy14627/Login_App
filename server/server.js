8ef3d7b8a9061bcbcd89f19e486aab05d47b0df2
Adarsh Mishra <adjee2020@gmail.com>
2024-02-14 03:19:20 +0530

    fifth  commit

--- server/server.js
+++ server/server.js
@@ -21,20 +21,20 @@ app.use(cors());
 app.use("/api/users", userRoutes);
 app.use("/api/upload/upload-cloudinary", cloudinaryUpload);
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+// const __dirname = path.resolve();
+// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/client/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+//   );
+// } else {
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+// }
 
 app.use(notFound);
 app.use(errorHandler);
